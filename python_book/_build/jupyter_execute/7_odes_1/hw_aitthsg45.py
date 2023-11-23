2a7276df9ba5e3b2c86f799cd427a0a3c5727b21
Peter Bratby <p.bratby@ucl.ac.uk>
2023-11-23 13:06:26 +0000

    .

--- python_book/_build/jupyter_execute/7_odes_1/hw_aitthsg45.py
+++ python_book/_build/jupyter_execute/7_odes_1/hw_aitthsg45.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# # Week 7 Homework Solutions
+
+# In[100]:
 
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-x,y = np.meshgrid(np.arange(-2,2,.1),np.arange(-1,1.1,.1))
-dy_by_dx = np.sqrt(1-y**2)
-plt.quiver(x,y, 1,dy_by_dx)
+x = np.linspace(0,1,15)
+y = np.linspace(0,3,10)
 
-xp = np.linspace(-2, 2, 100)
-yp = np.sin(xp)
-                    
-plt.plot(xp, yp)
+X, Y = np.meshgrid(x, y)
 
+dy = 1/((Y-1)*(X+1)) # y-length of arrow is dy/dx = X - Y
+dx = np.ones(dy.shape) # x-length of arrow is 1
 
-# In[2]:
+plt.quiver(X, Y, dx, dy, angles="xy")
 
+y1 = 1 + np.sqrt(1/16 + 2 * np.log(x + 1))
 
-x,y = np.meshgrid(np.arange(-2,2,.1),np.arange(-1,3,.2))
-dy_by_dx = (2*x - 6*x*y)/(1 + x**2)
-plt.quiver(x,y,1,dy_by_dx)
+plt.plot(x, y1)
 
-xp = np.linspace(-2, 2, 100)
-yp = 1/3 + (5/3) / ((1 + xp**2)**3)
-                    
-plt.plot(xp, yp)
+y2 = 1 - np.sqrt(1/16 + 2 * np.log(x + 1))
+plt.plot(x, y2)
 
 
-# In[ ]:
+# In[102]:
 
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+x = np.linspace(0,0.6,15)
+y = np.linspace(0,50,10)
+
+X, Y = np.meshgrid(x, y)
+
+dy = 5*Y + np.exp(-2*X)/(Y**2)
+dx = np.ones(dy.shape)
+
+plt.quiver(X, Y, dx, dy, angles="xy")
+
+y1 = ((139/17)*np.exp(15*x) - 3*(np.exp(-2*x)))**(1/3)
+
+plt.plot(x, y1)
 
